9d0ce774cc3ce6bd1edf323e59bbce6e9e8a55b7
khalil-chermiti <khalil666chermiti@gmail.com>
2023-05-08 20:25:31 +0100

    chore: update candidate and application routes response return types

--- src/application/application.controller.ts
+++ src/application/application.controller.ts
@@ -1,12 +1,18 @@
 import { Request } from 'express';
+import { Application } from '@prisma/client';
 import { authJwt } from 'src/guards/jwtInterface';
 import { isAdmin } from 'src/guards/authorization.guard';
 import { ApplicationService } from './application.service';
 import { authGuard } from 'src/guards/authentication.guard';
-import { addApplicationOuputDTO } from './dto/addApplicationOutputDTO';
-import { RemoveApplicationOutputDTO } from './dto/removeApplicationOutput';
-import { AcceptApplicationOutputDTO } from './dto/acceptApplicaitonOutputDTO';
-import { Body, Controller, Delete, Post, Req, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  HttpException,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 
 @Controller('application')
 export class ApplicationController {
@@ -17,16 +23,26 @@ export class ApplicationController {
   public async addApplication(
     @Body('master_id') masterID: number,
     @Req() request: Request,
-  ): Promise<addApplicationOuputDTO> {
+  ): Promise<ResponseData<{ application: Application }>> {
     const auth = request['user'] as authJwt;
-    const application = await this.applicationService.addApplication({
-      candidate_id: parseInt(auth.id),
-      master_id: masterID,
-    });
-    return {
-      success: true,
-      data: application,
-    };
+    try {
+      const application = await this.applicationService.addApplication({
+        candidate_id: parseInt(auth.id),
+        master_id: masterID,
+      });
+      return {
+        success: true,
+        statusCode: 200,
+        data: { application: application },
+      };
+    } catch (error: any) {
+      if (error instanceof HttpException)
+        return {
+          success: false,
+          statusCode: error.getStatus(),
+          error: error.message,
+        };
+    }
   }
 
   @Delete()
@@ -34,17 +50,27 @@ export class ApplicationController {
   public async removeApplication(
     @Body('application_id') applicationID: number,
     @Req() request: Request,
-  ): Promise<RemoveApplicationOutputDTO> {
+  ): Promise<ResponseData<{ application: Application }>> {
     const auth = request['user'] as authJwt;
-    const application = await this.applicationService.removeApplication({
-      application_id: applicationID,
-      candidate_id: parseInt(auth.id),
-    });
+    try {
+      const application = await this.applicationService.removeApplication({
+        application_id: applicationID,
+        candidate_id: parseInt(auth.id),
+      });
 
-    return {
-      success: true,
-      data: application,
-    };
+      return {
+        success: true,
+        statusCode: 200,
+        data: { application: application },
+      };
+    } catch (error: any) {
+      if (error instanceof HttpException)
+        return {
+          success: false,
+          statusCode: error.getStatus(),
+          error: error.message,
+        };
+    }
   }
 
   @Post('accept')
@@ -52,15 +78,25 @@ export class ApplicationController {
   @UseGuards(authGuard)
   public async acceptApplication(
     @Body('application_id') applicationID: number,
-  ): Promise<AcceptApplicationOutputDTO> {
-    const application = await this.applicationService.acceptApplication(
-      applicationID,
-    );
+  ): Promise<ResponseData<{ application: Application }>> {
+    try {
+      const application = await this.applicationService.acceptApplication(
+        applicationID,
+      );
 
-    return {
-      success: true,
-      data: application,
-    };
+      return {
+        success: true,
+        statusCode: 200,
+        data: { application: application },
+      };
+    } catch (error: any) {
+      if (error instanceof HttpException)
+        return {
+          success: false,
+          statusCode: error.getStatus(),
+          error: error.message,
+        };
+    }
   }
 
   @Post('reject')
@@ -68,14 +104,24 @@ export class ApplicationController {
   @UseGuards(authGuard)
   public async rejectApplication(
     @Body('application_id') applicationID: number,
-  ): Promise<AcceptApplicationOutputDTO> {
-    const application = await this.applicationService.rejectApplication(
-      applicationID,
-    );
+  ): Promise<ResponseData<{ application: Application }>> {
+    try {
+      const application = await this.applicationService.rejectApplication(
+        applicationID,
+      );
 
-    return {
-      success: true,
-      data: application,
-    };
+      return {
+        success: true,
+        statusCode: 200,
+        data: { application: application },
+      };
+    } catch (error: any) {
+      if (error instanceof HttpException)
+        return {
+          success: false,
+          statusCode: error.getStatus(),
+          error: error.message,
+        };
+    }
   }
 }
--- src/candidate/candidate.controller.ts
+++ src/candidate/candidate.controller.ts
@@ -1,11 +1,17 @@
+import { Candidate } from '@prisma/client';
 import { SignUpInputDTO } from './dto/signupInputDTO';
 import { SigninInputDTO } from './dto/signinInputDTO';
 import { CandidateService } from './candidate.service';
-import { SignupOutputDTO } from './dto/signupOutputDTO';
-import { SigninOutputDTO } from './dto/signinOutputDTO';
 import { authGuard } from 'src/guards/authentication.guard';
 import { isCandidate } from 'src/guards/authorization.guard';
-import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpException,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 
 @Controller('candidate')
 export class CandidateController {
@@ -14,28 +20,47 @@ export class CandidateController {
   @Post('signup')
   public async signup(
     @Body() signupInputDTO: SignUpInputDTO,
-  ): Promise<SignupOutputDTO> {
-    const candidate = await this.candidateService.signup(signupInputDTO);
-    delete candidate.password;
-
-    return {
-      success: true,
-      data: candidate,
-    };
+  ): Promise<ResponseData<{ candidate: Omit<Candidate, 'password'> }>> {
+    try {
+      const candidate = await this.candidateService.signup(signupInputDTO);
+      delete candidate.password;
+      return {
+        success: true,
+        statusCode: 200,
+        data: { candidate: candidate },
+      };
+    } catch (error: any) {
+      if (error instanceof HttpException)
+        return {
+          success: false,
+          statusCode: error.getStatus(),
+          error: error.message,
+        };
+    }
   }
 
   @Post('signin')
   public async signin(
     @Body() signinInputDTO: SigninInputDTO,
-  ): Promise<SigninOutputDTO> {
-    const token = await this.candidateService.signin(signinInputDTO);
+  ): Promise<ResponseData<{ token: string }>> {
+    try {
+      const token = await this.candidateService.signin(signinInputDTO);
 
-    return {
-      success: true,
-      data: {
-        token: token,
-      },
-    };
+      return {
+        success: true,
+        statusCode: 200,
+        data: {
+          token: token,
+        },
+      };
+    } catch (error: any) {
+      if (error instanceof HttpException)
+        return {
+          success: false,
+          statusCode: error.getStatus(),
+          error: error.message,
+        };
+    }
   }
 
   @Get('test')
